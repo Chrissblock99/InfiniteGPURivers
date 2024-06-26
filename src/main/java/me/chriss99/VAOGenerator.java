5ed594b1a7eadbf7da1b35507719bdf52aee58e9
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-06-26 20:32:47 +0200

    add new HeightMap presets

--- src/main/java/me/chriss99/VAOGenerator.java
+++ src/main/java/me/chriss99/VAOGenerator.java
@@ -21,6 +21,27 @@ public class VAOGenerator {
         return heights;
     }
 
+    public static double[][] pillars(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        for (int x = 0; x < xSize; x++)
+            for (int z = 0; z < zSize; z++)
+                heights[x][z] =
+                        (x > xSize*0.6 && x < xSize*0.7 && z > zSize*0.3 && z < zSize*0.4) ||
+                        (x > xSize*0.45 && x < xSize*0.55 && z > zSize*0.45 && z < zSize*0.55) ||
+                        (x > xSize*0.3 && x < xSize*0.4 && z > zSize*0.6 && z < zSize*0.7) ? 120 : 0;
+
+        return heights;
+    }
+
+    public static double[][] spike(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        heights[50][50] = 30;
+
+        return heights;
+    }
+
     public static VAO heightMapToSimpleVAO(double[][] heightMap) {
         double[] vertecies = new double[heightMap.length*heightMap[0].length*3];
         double[] color = new double[heightMap.length*heightMap[0].length*3];
