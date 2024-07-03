1c1f5d075f55983bc12843f22202cc9d8e6fadec
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-07-03 21:31:24 +0200

    move heightMap generators into a new HeightMapGenerator class

---
+++ src/main/java/me/chriss99/HeightMapGenerator.java
@@ -0,0 +1,44 @@
+package me.chriss99;
+
+public class HeightMapGenerator {
+    public static double[][] randomHeights(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        for (int x = 0; x < xSize; x++)
+            for (int z = 0; z < zSize; z++)
+                heights[x][z] = Math.random()*3;
+
+        return heights;
+    }
+
+    public static double[][] pillar(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        for (int x = 0; x < xSize; x++)
+            for (int z = 0; z < zSize; z++)
+                heights[x][z] = (x > xSize*0.4 && x < xSize*0.6 && z > zSize*0.4 && z < zSize*0.6) ? 30 : 0;
+
+        return heights;
+    }
+
+    public static double[][] pillars(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        for (int x = 0; x < xSize; x++)
+            for (int z = 0; z < zSize; z++)
+                heights[x][z] =
+                        (x > xSize*0.6 && x < xSize*0.7 && z > zSize*0.3 && z < zSize*0.4) ||
+                        (x > xSize*0.45 && x < xSize*0.55 && z > zSize*0.45 && z < zSize*0.55) ||
+                        (x > xSize*0.3 && x < xSize*0.4 && z > zSize*0.6 && z < zSize*0.7) ? 120 : 0;
+
+        return heights;
+    }
+
+    public static double[][] spike(int xSize, int zSize) {
+        double[][] heights = new double[xSize][zSize];
+
+        heights[50][50] = 30;
+
+        return heights;
+    }
+}
