10732a6f79045e57d7b9863999295efc00ea8b53
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-08-16 12:26:26 +0200

    move compute shaders into a compute package

--- src/main/java/me/chriss99/ComputeProgram.java
+++ src/main/java/me/chriss99/ComputeProgram.java
@@ -4,7 +4,7 @@ import static org.lwjgl.opengl.GL45.*;
 
 public class ComputeProgram extends GLProgram {
     public ComputeProgram(String name) {
-        addShader( name + ".comp", GL_COMPUTE_SHADER);
+        addShader( "compute/" + name + ".comp", GL_COMPUTE_SHADER);
         validate();
     }
 }
