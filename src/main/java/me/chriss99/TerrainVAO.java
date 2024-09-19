6657c20077082c1baf27bb5aac9bbe0e29f818d9
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-09-19 20:59:25 +0200

    add a TerrainVAO class and a generator for it

---
+++ src/main/java/me/chriss99/TerrainVAO.java
@@ -0,0 +1,54 @@
+package me.chriss99;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL30.*;
+
+public class TerrainVAO {
+    private final int vao;
+    private final int coordVBO;
+    private final int indicesVBO;
+    private final int indexLength;
+
+    public TerrainVAO(float[] triangle, int[] index) {
+        this.vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+
+        ByteBuffer vertices = Util.storeArrayInBuffer(triangle);
+        ByteBuffer indices = Util.storeArrayInBuffer(index);
+
+        coordVBO = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, coordVBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+
+        indicesVBO = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesVBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        indexLength = index.length;
+    }
+
+    public void updatePositions(float[] positions) {
+        glBindBuffer(GL_ARRAY_BUFFER, coordVBO);
+        ByteBuffer positionBuffer = Util.storeArrayInBuffer(positions);
+        glBufferData(GL_ARRAY_BUFFER, positionBuffer, GL_STREAM_DRAW);
+    }
+
+    public void bind() {
+        glBindVertexArray(vao);
+    }
+
+    public int indexLength() {
+        return indexLength;
+    }
+
+    public void delete() {
+        glDeleteBuffers(coordVBO);
+        glDeleteBuffers(indicesVBO);
+        glDeleteVertexArrays(vao);
+    }
+}
---
+++ src/main/java/me/chriss99/TerrainVAOGenerator.java
@@ -0,0 +1,48 @@
+package me.chriss99;
+
+public class TerrainVAOGenerator {
+    public static float[] heightMapToSimpleVertexes(float[][] heightMap) {
+        float[] vertecies = new float[heightMap.length*heightMap[0].length*3];
+        int vertexShift = 0;
+
+        for (int z = 0; z < heightMap[0].length; z++)
+            for (int x = 0; x < heightMap.length; x++) {
+                vertecies[vertexShift] = x;
+                vertecies[vertexShift + 1] = heightMap[x][z];
+                vertecies[vertexShift + 2] = z;
+
+                vertexShift += 3;
+            }
+
+        return vertecies;
+    }
+
+    public static int[] heightMapToSimpleIndex(int width, int height) {
+        int[] index = new int[(width-1)*(height-1)*6];
+        int indexShift = 0;
+
+        for (int z = 0; z < height; z++)
+            for (int x = 0; x < width; x++) {
+
+                if (z == height-1 || x == width-1)
+                    continue;
+
+                index[indexShift+0] = Util.indexOfXZFlattenedArray(x, z, height);
+                index[indexShift+1] = Util.indexOfXZFlattenedArray(x+1, z, height);
+                index[indexShift+2] = Util.indexOfXZFlattenedArray(x, z+1, height);
+                index[indexShift+3] = Util.indexOfXZFlattenedArray(x+1, z, height);
+                index[indexShift+4] = Util.indexOfXZFlattenedArray(x+1, z+1, height);
+                index[indexShift+5] = Util.indexOfXZFlattenedArray(x, z+1, height);
+                indexShift += 6;
+            }
+
+        return index;
+    }
+
+    public static TerrainVAO heightMapToSimpleVAO(float[][] heightMap) {
+        float[] vertexes = heightMapToSimpleVertexes(heightMap);
+        int[] index = heightMapToSimpleIndex(heightMap.length, heightMap[0].length);
+
+        return new TerrainVAO(vertexes, index);
+    }
+}
