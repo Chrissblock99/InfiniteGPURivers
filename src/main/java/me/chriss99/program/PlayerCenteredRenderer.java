316086f4b7d5c533b532c7daaec6e701fed0a4bd
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-10-09 13:36:43 +0200

    add force update functionality to PlayerCenteredRenderer

--- src/main/java/me/chriss99/program/PlayerCenteredRenderer.java
+++ src/main/java/me/chriss99/program/PlayerCenteredRenderer.java
@@ -29,19 +29,24 @@ public class PlayerCenteredRenderer extends TerrainVAOMapProgram {
     }
 
     public void updateLoadedChunks(Vector2i skipSrcPos, Vector2i skipSideLength) {
+        updateLoadedChunks(skipSrcPos, skipSideLength, false);
+    }
+
+    public void updateLoadedChunks(Vector2i skipSrcPos, Vector2i skipSideLength, boolean force) {
         Vector2f position = new Vector2f();
         position.x = cameraMatrix.position.x - (chunkRenderDistance-1)*64;
         position.y = cameraMatrix.position.z - (chunkRenderDistance-1)*64;
         position.div(64f).floor().mul(64f);
 
-        if (position.equals(previousPosition))
+        if (position.equals(previousPosition) && !force)
             return;
         previousPosition = position;
 
         int sideLength = chunkRenderDistance*2-1;
         Vector2i srcPos = new Vector2i((int) position.x, (int) position.y);
 
-        terrainVAOs.values().removeIf(terrainVAO -> !pointInsideRectangle(terrainVAO.srcPos, srcPos, new Vector2i(sideLength*64)));
+        terrainVAOs.values().removeIf(terrainVAO -> !pointInsideRectangle(terrainVAO.srcPos, srcPos, new Vector2i(sideLength*64))
+                || pointInsideRectangle(terrainVAO.srcPos, skipSrcPos, skipSideLength));
         for (int x = 0; x < sideLength; x++)
             for (int y= 0; y < sideLength; y++) {
                 Vector2i pos = new Vector2i((int) position.x + x*64, (int) position.y + y*64);
