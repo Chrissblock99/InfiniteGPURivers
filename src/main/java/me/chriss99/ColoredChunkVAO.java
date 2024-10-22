4fd93e7f40ea0f822212e6e25ae4026ed98526e7
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-10-22 11:17:53 +0200

    ColoredChunkVAO now extends ColoredVAO

--- src/main/java/me/chriss99/ColoredChunkVAO.java
+++ src/main/java/me/chriss99/ColoredChunkVAO.java
@@ -2,13 +2,12 @@ package me.chriss99;
 
 import org.joml.Vector2i;
 
-public class ColoredChunkVAO implements ChunkVAO {
-    private final ColoredVAO vao;
+public class ColoredChunkVAO extends ColoredVAO implements ChunkVAO {
     private final Vector2i srcPos;
     private final int width;
 
-    public ColoredChunkVAO(ColoredVAO vao, Vector2i srcPos, int width) {
-        this.vao = vao;
+    public ColoredChunkVAO(double[] triangle, double[] color, int[] index, Vector2i srcPos, int width) {
+        super(triangle, color, index);
         this.srcPos = srcPos;
         this.width = width;
     }
@@ -21,20 +20,5 @@ public class ColoredChunkVAO implements ChunkVAO {
     @Override
     public int getWidth() {
         return width;
-    }
-
-    @Override
-    public int getIndexLength() {
-        return vao.getIndexLength();
-    }
-
-    @Override
-    public void bind() {
-        vao.bind();
-    }
-
-    @Override
-    public void delete() {
-        vao.delete();
     }
 }
