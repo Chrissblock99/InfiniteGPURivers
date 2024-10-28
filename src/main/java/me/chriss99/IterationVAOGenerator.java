1af265bbe11a49a2642c78e4427237e8dbc67c09
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2024-10-28 12:46:48 +0100

    merge IterationVAOGenerator functions into a single loop

--- src/main/java/me/chriss99/IterationVAOGenerator.java
+++ src/main/java/me/chriss99/IterationVAOGenerator.java
@@ -3,9 +3,12 @@ package me.chriss99;
 import org.joml.Vector2i;
 
 public class IterationVAOGenerator {
-    public static float[] heightMapToIterationVertexes(Vector2i srcPosInChunks, Vector2i sizeInChunks, ErosionDataStorage data) {
+    public static IterationVAO heightMapToIterationVAO(Vector2i srcPosInChunks, Vector2i sizeInChunks, ErosionDataStorage data) {
         float[] vertecies = new float[sizeInChunks.x* sizeInChunks.y];
+        int[] index = new int[(sizeInChunks.x-1)*(sizeInChunks.y-1)*6];
+
         int vertexShift = 0;
+        int indexShift = 0;
 
         for (int z = 0; z < sizeInChunks.y; z++)
             for (int x = 0; x < sizeInChunks.x; x++) {
@@ -13,37 +16,20 @@ public class IterationVAOGenerator {
                 vertecies[vertexShift] = data.iterationOf(position) + data.iterationSurfaceTypeOf(position).getSurface()[0][0]* data.chunkSize;
 
                 vertexShift++;
-            }
 
-        return vertecies;
-    }
 
-    public static int[] heightMapToSimpleIndex(Vector2i size) {
-        int[] index = new int[(size.x-1)*(size.y-1)*6];
-        int indexShift = 0;
-
-        for (int z = 0; z < size.y; z++)
-            for (int x = 0; x < size.x; x++) {
-
-                if (z == size.y-1 || x == size.x-1)
+                if (z == sizeInChunks.y-1 || x == sizeInChunks.x-1)
                     continue;
 
-                index[indexShift+0] = Util.indexOfXZFlattenedArray(x, z, size.x);
-                index[indexShift+1] = Util.indexOfXZFlattenedArray(x+1, z, size.x);
-                index[indexShift+2] = Util.indexOfXZFlattenedArray(x, z+1, size.x);
-                index[indexShift+3] = Util.indexOfXZFlattenedArray(x+1, z, size.x);
-                index[indexShift+4] = Util.indexOfXZFlattenedArray(x+1, z+1, size.x);
-                index[indexShift+5] = Util.indexOfXZFlattenedArray(x, z+1, size.x);
+                index[indexShift+0] = Util.indexOfXZFlattenedArray(x, z, sizeInChunks.x);
+                index[indexShift+1] = Util.indexOfXZFlattenedArray(x+1, z, sizeInChunks.x);
+                index[indexShift+2] = Util.indexOfXZFlattenedArray(x, z+1, sizeInChunks.x);
+                index[indexShift+3] = Util.indexOfXZFlattenedArray(x+1, z, sizeInChunks.x);
+                index[indexShift+4] = Util.indexOfXZFlattenedArray(x+1, z+1, sizeInChunks.x);
+                index[indexShift+5] = Util.indexOfXZFlattenedArray(x, z+1, sizeInChunks.x);
                 indexShift += 6;
             }
 
-        return index;
-    }
-
-    public static IterationVAO heightMapToIterationVAO(Vector2i srcPosInChunks, Vector2i sizeInChunks, ErosionDataStorage data) {
-        float[] vertexes = heightMapToIterationVertexes(srcPosInChunks, sizeInChunks, data);
-        int[] index = heightMapToSimpleIndex(sizeInChunks);
-
-        return new IterationVAO(vertexes, index, srcPosInChunks, sizeInChunks.x);
+        return new IterationVAO(vertecies, index, srcPosInChunks, sizeInChunks.x);
     }
 }
