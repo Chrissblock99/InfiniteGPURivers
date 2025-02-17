2df5dc07b49e3b59cffd3f33dd591c75869e0878
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-02-17 23:14:30 +0100

    improve erosion management

--- src/main/java/me/chriss99/ErosionManager.java
+++ src/main/java/me/chriss99/ErosionManager.java
@@ -17,36 +17,50 @@ public class ErosionManager {
     }
 
     public boolean findIterate(Vector2i pos, int max) {
-        for (int x = 0; x < max; x = (x>=0) ? -x-1 : -x)
-            for (int y = 0; y < max; y = (y>=0) ? -y-1 : -y)
-                if (iterate(new Vector2i(x, y).add(pos)))
-                    return true;
+        Vector2i size = new Vector2i(maxChunks);
+
+        while (size.x > 1) {
+            for (int x = 0; x < max; x = (x>=0) ? -x-1 : -x)
+                for (int y = 0; y < max; y = (y>=0) ? -y-1 : -y)
+                    if (iterate(new Vector2i(x, y).add(pos), size))
+                        return true;
+            size.sub(1, 1);
+        }
+
         return false;
     }
 
-    public boolean iterate(Vector2i pos) {
+    public boolean iterate(Vector2i pos, Vector2i size) {
         int x = pos.x;
         int y = pos.y;
 
-        int l = data.getTile(x, y+1).horizontal;
-        int r = data.getTile(x+1, y+1).horizontal;
-        int f = data.getTile(x+1, y+1).vertical;
-        int b = data.getTile(x+1, y).vertical;
+        Vector2i length = new Vector2i(size).sub(1, 1);
+
+        int l = getEdgesEqual(pos, length.y, true);
+        int r = getEdgesEqual(new Vector2i(pos).add(length.x,0), length.y, true);
+        int f = getEdgesEqual(new Vector2i(pos).add(0,length.y), length.x, false);
+        int b = getEdgesEqual(pos, length.x, false);
+
+        if (l > 1 || r > 1 || f > 1 || b > 1)
+            return false;
 
         if (l == -1 || r == 1 || f == 1 || b == -1)
             return false;
 
         IterationSurfaceType.SurfaceType allowed = IterationSurfaceType.SurfaceType.FLAT;
-        boolean tl = allowed.equals(data.getIterationSurfaceType(x, y+1).getSurfaceType());
-        boolean tr = allowed.equals(data.getIterationSurfaceType(x+1, y+1).getSurfaceType());
+        boolean tl = allowed.equals(data.getIterationSurfaceType(x, y+length.y).getSurfaceType());
+        boolean tr = allowed.equals(data.getIterationSurfaceType(x+length.x, y+length.y).getSurfaceType());
         boolean dl = allowed.equals(data.getIterationSurfaceType(x, y).getSurfaceType());
-        boolean dr = allowed.equals(data.getIterationSurfaceType(x+1, y).getSurfaceType());
+        boolean dr = allowed.equals(data.getIterationSurfaceType(x+length.x, y).getSurfaceType());
 
         if (l == 0 && f == 0 && !tl || f == 0 && r == 0 && !tr || l == 0 && b == 0 && !dl || b == 0 && r == 0 && !dr)
             return false;
 
+        if (!isFlat(new Vector2i(pos).add(1, 1), new Vector2i(size).sub(2, 2)))
+            return false;
 
-        eroder.changeArea(new Vector2i(x, y).mul(data.chunkSize), new Vector2i(data.chunkSize).mul(2));
+
+        eroder.changeArea(new Vector2i(x, y).mul(data.chunkSize), new Vector2i(data.chunkSize).mul(size));
         eroder.erosionSteps(data.chunkSize, l == 0, r == 0, f == 0, b == 0);
 
         l--;
@@ -54,11 +68,48 @@ public class ErosionManager {
         f++;
         b--;
 
-        data.getTile(x, y+1).horizontal = l;
-        data.getTile(x+1, y+1).horizontal = r;
-        data.getTile(x+1, y+1).vertical = f;
-        data.getTile(x+1, y).vertical = b;
+        setEdges(pos, length.y, true, l);
+        setEdges(new Vector2i(pos).add(length.x,0), length.y, true, r);
+        setEdges(new Vector2i(pos).add(0,length.y), length.x, false, f);
+        setEdges(pos, length.x, false, b);
 
         return true;
+    }
+
+    private boolean isFlat(Vector2i pos, Vector2i size) {
+        for (int x = 0; x < size.x; x++)
+            for (int y = 0; y < size.y; y++)
+                if (!data.getIterationSurfaceType(pos.x + x, pos.y + y).getSurfaceType().equals(IterationSurfaceType.SurfaceType.FLAT))
+                    return false;
+        return true;
+    }
+
+    /**
+     *
+     * @param pos where to start checking for equality
+     * @param length how far to check for equality
+     * @param upwards whether to check for equality upwards or sideways
+     * @return -1, 0 or 1 if all are equal, else the length at which it isn't equal (larger than 1)
+     */
+    private int getEdgesEqual(Vector2i pos, int length, boolean upwards) {
+        int edge = getEdge(pos, 1, upwards);
+
+        for (int i = 2; i <= length; i++)
+            if (getEdge(pos, i, upwards) != edge)
+                return i;
+
+        return edge;
+    }
+
+    private int getEdge(Vector2i pos, int offset, boolean upwards) {
+        return upwards ? data.getTile(pos.x, pos.y+offset).horizontal : data.getTile(pos.x+offset, pos.y).vertical;
+    }
+
+    private void setEdges(Vector2i pos, int length, boolean upwards, int value) {
+        for (int i = 1; i <= length; i++)
+            if (upwards)
+                data.getTile(pos.x, pos.y+i).horizontal = value;
+            else
+                data.getTile(pos.x+i, pos.y).vertical = value;
     }
 }
