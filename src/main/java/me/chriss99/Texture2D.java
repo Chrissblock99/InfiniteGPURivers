5b0ae3a3147a51c36db040440f8f34e8c28080dc
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-03-07 01:30:42 +0100

    fix Texture2D not binding itself properly

--- src/main/java/me/chriss99/Texture2D.java
+++ src/main/java/me/chriss99/Texture2D.java
@@ -18,6 +18,7 @@ public class Texture2D implements GLObject {
         this.size = new Vector2i(size);
 
         texture = glGenTextures();
+        bind();
         glTexStorage2D(GL_TEXTURE_2D, 1, internalFormat, size.x, size.y);
     }
 
@@ -35,11 +36,13 @@ public class Texture2D implements GLObject {
     }
 
     public void downloadData(Vector2i offset, Array2DBufferWrapper writeTo) {
+        bind();
         //excuse me the docs say that I have to use "GL_TEXTURE_2D" instead of "texture"
         glGetTextureSubImage(texture, 0, offset.x, offset.y, 0, writeTo.getSize().x, writeTo.getSize().y, 1, writeTo.type.glFormat, writeTo.type.glType, writeTo.buffer);
     }
 
     public void downloadFullData(int format, int type, ByteBuffer buffer) {
+        bind();
         glGetTexImage(GL_TEXTURE_2D, 0, format, type, buffer);
     }
 
