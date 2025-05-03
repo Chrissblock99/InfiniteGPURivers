54cacd9b7788c1c9947d3cfea78481ca44b1069a
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-03 21:02:29 +0200

    use kotlin features in GLProgram

--- src/main/kotlin/program/GLProgram.kt
+++ src/main/kotlin/program/GLProgram.kt
@@ -6,23 +6,16 @@ import java.io.FileNotFoundException
 import java.util.*
 
 open class GLProgram {
-    val program: Int
+    val program: Int = glCreateProgram()
     private val shaders = ArrayList<Int>()
-
-    init {
-        this.program = glCreateProgram()
-    }
 
     fun addShader(name: String, type: Int) {
         val shader = loadShader(File("src/main/glsl/$name"), type)
-        if (glGetShaderi(shader, GL_COMPILE_STATUS) !== 1) println("Shader $name did not compile!")
+        if (glGetShaderi(shader, GL_COMPILE_STATUS) != 1)
+            System.err.println("Shader $name did not compile!")
 
         shaders.add(shader)
         glAttachShader(program, shader)
-    }
-
-    fun bindAttribute(index: Int, name: String) {
-        glBindAttribLocation(program, index, name)
     }
 
     fun validate() {
@@ -32,55 +25,43 @@ open class GLProgram {
         val link: Int = glGetProgrami(program, GL_LINK_STATUS)
         val validate: Int = glGetProgrami(program, GL_VALIDATE_STATUS)
 
-        if (link == 1 && validate == 1) return
+        if (link == 1 && validate == 1)
+            return
 
         println("Program Linked: $link")
         println("Program Validated: $validate")
     }
 
-    fun use() {
-        glUseProgram(program)
-    }
-
-    fun getUniform(name: String): Int {
-        return glGetUniformLocation(program, name)
-    }
+    fun bindAttribute(index: Int, name: String) = glBindAttribLocation(program, index, name)
+    fun use() = glUseProgram(program)
+    fun getUniform(name: String) = glGetUniformLocation(program, name)
 
     open fun delete() {
-        //detach the shaders from the program object
         for (shader in shaders) {
             glDetachShader(program, shader)
             glDeleteShader(shader)
         }
 
-        //stop using the shader program
         glUseProgram(0)
-
-        //delete the program now that the shaders are detached and the program isn't being used
         glDeleteProgram(program)
     }
 
     companion object {
         fun loadShader(file: File, type: Int): Int {
-            try {
-                val sc = Scanner(file)
-                val data = StringBuilder()
+            val sc = Scanner(file)
+            val data = StringBuilder()
 
-                if (file.exists()) {
-                    while (sc.hasNextLine()) {
-                        data.append(sc.nextLine()).append("\n")
-                    }
-
-                    sc.close()
+            if (file.exists()) {
+                while (sc.hasNextLine()) {
+                    data.append(sc.nextLine()).append("\n")
                 }
-                val id: Int = glCreateShader(type)
-                glShaderSource(id, data)
-                glCompileShader(id)
-                return id
-            } catch (e: FileNotFoundException) {
-                e.printStackTrace()
-                return -1
+
+                sc.close()
             }
+            val id: Int = glCreateShader(type)
+            glShaderSource(id, data)
+            glCompileShader(id)
+            return id
         }
     }
 }
