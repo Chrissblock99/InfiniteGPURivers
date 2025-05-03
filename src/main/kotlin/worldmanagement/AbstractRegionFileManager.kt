1b94952801b50dccac28f0063bf2fec59e373907
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-03 23:03:42 +0200

    use kotlin features in AbstractRegionFileManager

--- src/main/kotlin/worldmanagement/AbstractRegionFileManager.kt
+++ src/main/kotlin/worldmanagement/AbstractRegionFileManager.kt
@@ -3,30 +3,15 @@ package me.chriss99.worldmanagement
 import glm_.vec2.Vec2i
 
 abstract class AbstractRegionFileManager<T>(worldName: String) : RegionFileManager<T> {
-    private val fileManager: FileLoadStoreManager<Region<T>>
+    private val fileManager: FileLoadStoreManager<Region<T>> = FileLoadStoreManager(
+        "worlds/$worldName", "quadtree",
+        ::regionFromBytes, ::regionToBytes
+    )
 
-    init {
-        fileManager = FileLoadStoreManager(
-            "worlds/$worldName",
-            "quadtree",
-            { bytes: ByteArray, pos: Vec2i -> this.regionFromBytes(bytes, pos) },
-            { region: Region<T> -> this.regionToBytes(region) })
-    }
-
-    override fun hasFile(key: Vec2i): Boolean {
-        return true
-    }
-
-    override fun loadFile(chunkCoord: Vec2i): Region<T> {
-        return fileManager.loadFile(chunkCoord)
-    }
-
-    override fun saveFile(pos: Vec2i, region: Region<T>) {
-        fileManager.saveFile(region, region.coord)
-    }
-
+    override fun hasFile(key: Vec2i) = true
+    override fun loadFile(chunkCoord: Vec2i) = fileManager.loadFile(chunkCoord)
+    override fun saveFile(pos: Vec2i, region: Region<T>) = fileManager.saveFile(region, region.coord)
 
     protected abstract fun regionFromBytes(bytes: ByteArray, pos: Vec2i): Region<T>
-
     protected abstract fun regionToBytes(region: Region<T>): ByteArray
 }
