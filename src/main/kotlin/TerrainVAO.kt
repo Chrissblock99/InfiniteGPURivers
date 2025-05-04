3ac94c8c3b61fc76454680259a7dcf71acc6297b
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-04 17:10:57 +0200

    use kotlin features in TerrainVAO

--- src/main/kotlin/TerrainVAO.kt
+++ src/main/kotlin/TerrainVAO.kt
@@ -3,32 +3,20 @@ package me.chriss99
 import me.chriss99.glabstractions.VAOImpl
 import glm_.vec2.Vec2i
 
-class TerrainVAO(triangle: FloatArray, index: IntArray?, srcPos: Vec2i, width: Int, scale: Int) :
+class TerrainVAO(
+    triangle: FloatArray, index: IntArray?,
+     override val srcPos: Vec2i,
+     override val width: Int,
+     val scale: Int
+) :
     ChunkVAO {
-    private val vao: VAOImpl
-    override val srcPos: Vec2i get() = Vec2i(field)
-    override val width: Int
-    val scale: Int
+    private val vao: VAOImpl = VAOImpl(index, 2, triangle)
 
-    init {
-        vao = VAOImpl(index, 2, triangle)
-        this.srcPos = srcPos
-        this.width = width
-        this.scale = scale
-    }
-
-    fun updatePositions(positions: FloatArray) {
-        vao.updateVertices(0, positions)
-    }
+    fun updatePositions(positions: FloatArray) = vao.updateVertices(0, positions)
 
     override val indexLength: Int
         get() = vao.indexLength
 
-    override fun bind() {
-        vao.bind()
-    }
-
-    override fun delete() {
-        vao.delete()
-    }
+    override fun bind() = vao.bind()
+    override fun delete() = vao.delete()
 }
