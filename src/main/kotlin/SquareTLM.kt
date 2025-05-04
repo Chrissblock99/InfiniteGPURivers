3f8311b89158ee78d9ac2ff2476c858b36ca715f
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-04 17:22:00 +0200

    use kotlin features in SquareTLM

--- src/main/kotlin/SquareTLM.kt
+++ src/main/kotlin/SquareTLM.kt
@@ -5,22 +5,17 @@ import me.chriss99.worldmanagement.TileLoadManager
 import glm_.vec2.Vec2
 import glm_.vec2.Vec2i
 
-open class SquareTLM<T>(var renderDistance: Int, initPos: Vec2) : TileLoadManager<T> {
-    var position: Vec2 = Vec2(initPos)
-        set(value) {
-        field = Vec2(value)
-    }
-
+open class SquareTLM<T>(var renderDistance: Int, var position: Vec2) : TileLoadManager<T> {
     override fun loadPolicy(tilePos: Vec2i, tile: T): Boolean {
-        val distance: Vec2 = Vec2(tilePos).minus(position)
+        val distance = Vec2(tilePos) - position
         return distance.x.abs < renderDistance && distance.y.abs < renderDistance
     }
 
     override fun loadCommander(): Collection<Vec2i> {
-        val toLoad: ArrayList<Vec2i> = ArrayList<Vec2i>()
+        val toLoad: ArrayList<Vec2i> = ArrayList()
 
-        for (x in -renderDistance..<renderDistance + 1)
-            for (y in -renderDistance..<renderDistance + 1)
+        for (x in -renderDistance..renderDistance)
+            for (y in -renderDistance..renderDistance)
                 toLoad.add(Vec2i(position.x.toInt() + x, position.y.toInt() + y))
 
         return toLoad
