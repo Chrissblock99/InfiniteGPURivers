0a0fe0849f4b86212d7f3ac063bb2b2fe528f834
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-04 17:05:41 +0200

    use kotlin features in Window

--- src/main/kotlin/Window.kt
+++ src/main/kotlin/Window.kt
@@ -1,6 +1,6 @@
 package me.chriss99
 
-import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11.*
 
@@ -15,7 +15,7 @@ class Window {
     var vSync: Boolean = false
         set(vSync) {
             field = vSync
-            GLFW.glfwSwapInterval(if (vSync) 1 else 0)
+            glfwSwapInterval(if (vSync) 1 else 0)
         }
     var wireFrame: Boolean = false
         set(wireFrame) {
@@ -30,49 +30,50 @@ class Window {
         }
 
     init {
-        width = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())!!.width()
-        height = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())!!.height()
+        width = glfwGetVideoMode(glfwGetPrimaryMonitor())!!.width()
+        height = glfwGetVideoMode(glfwGetPrimaryMonitor())!!.height()
 
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4)
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 5)
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE)
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE)
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5)
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
 
-        windowId = GLFW.glfwCreateWindow(width, height, "GLFW OpenGL Window", 0, 0)
+        windowId = glfwCreateWindow(width, height, "GLFW OpenGL Window", 0, 0)
 
-        GLFW.glfwMakeContextCurrent(windowId)
-        if (!GL.createCapabilities().OpenGL45) System.err.println(
+        glfwMakeContextCurrent(windowId)
+        if (!GL.createCapabilities().OpenGL45)
+            System.err.println(
             """
-                    -----------------------------------------
-                    
-                    This device does not support OpenGL 4.5!
-                    Parts of the program may still work,
-                    but expect issues like things not
-                    rendering or just crashing.
-                    
-                    -----------------------------------------
-                    
-                    """.trimIndent()
+            -----------------------------------------
+            
+            This device does not support OpenGL 4.5!
+            Parts of the program may still work,
+            but expect issues like things not
+            rendering or just crashing.
+            
+            -----------------------------------------
+            
+            """.trimIndent()
         )
         glViewport(0, 0, width, height)
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
         glEnable(GL_DEPTH_TEST)
 
         inputDeviceManager = InputDeviceManager(windowId)
-        GLFW.glfwSetInputMode(windowId, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED)
+        glfwSetInputMode(windowId, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
 
         vSync = true
         wireFrame = false
 
-        GLFW.glfwShowWindow(windowId)
+        glfwShowWindow(windowId)
     }
 
     fun updateWindowSize() {
         val width = IntArray(1)
         val height = IntArray(1)
 
-        GLFW.glfwGetWindowSize(windowId, width, height)
+        glfwGetWindowSize(windowId, width, height)
 
         this.width = width[0]
         this.height = height[0]
@@ -83,19 +84,8 @@ class Window {
     val aspectRatio: Float
         get() = height.toFloat() / width.toFloat()
 
-    fun shouldClose(): Boolean {
-        return GLFW.glfwWindowShouldClose(windowId)
-    }
-
-    fun swapBuffers() {
-        GLFW.glfwSwapBuffers(windowId)
-    }
-
-    fun clearBuffers() {
-        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-    }
-
-    fun pollEvents() {
-        GLFW.glfwPollEvents()
-    }
+    fun shouldClose() = glfwWindowShouldClose(windowId)
+    fun swapBuffers() = glfwSwapBuffers(windowId)
+    fun clearBuffers() = glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+    fun pollEvents() = glfwPollEvents()
 }
