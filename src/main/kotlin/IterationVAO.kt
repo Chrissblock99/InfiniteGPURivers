0b620671d042fad8ea4be384761875915c536dc3
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-05-04 16:58:06 +0200

    use kotlin features in IterationVAO

--- src/main/kotlin/IterationVAO.kt
+++ src/main/kotlin/IterationVAO.kt
@@ -3,29 +3,14 @@ package me.chriss99
 import me.chriss99.glabstractions.VAOImpl
 import glm_.vec2.Vec2i
 
-class IterationVAO(triangle: FloatArray, srcPos: Vec2i, width: Int) : ChunkVAO {
-    private val vao: VAOImpl
-    override val srcPos: Vec2i get() = Vec2i(field)
-    override val width: Int
+class IterationVAO(triangle: FloatArray, override val srcPos: Vec2i, override val width: Int) : ChunkVAO {
+    private val vao: VAOImpl = VAOImpl(null, 3, triangle)
 
-    init {
-        vao = VAOImpl(null, 3, triangle)
-        this.srcPos = srcPos
-        this.width = width
-    }
-
-    fun updatePositions(positions: FloatArray) {
-        vao.updateVertices(0, positions)
-    }
+    fun updatePositions(positions: FloatArray) = vao.updateVertices(0, positions)
 
     override val indexLength: Int
         get() = vao.indexLength
 
-    override fun bind() {
-        vao.bind()
-    }
-
-    override fun delete() {
-        vao.delete()
-    }
+    override fun bind() = vao.bind()
+    override fun delete() = vao.delete()
 }
