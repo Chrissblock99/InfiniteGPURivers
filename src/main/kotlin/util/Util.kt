a5b0985a1fe4f4917bf08c2f734915d84dd81293
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-08-23 20:59:18 +0200

    add spiral functions to Util

--- src/main/kotlin/util/Util.kt
+++ src/main/kotlin/util/Util.kt
@@ -3,7 +3,9 @@ package me.chriss99.util
 import glm_.vec2.Vec2i
 import org.lwjgl.BufferUtils
 import java.nio.ByteBuffer
+import kotlin.math.ceil
 import kotlin.math.floor
+import kotlin.math.sqrt
 
 object Util {
     fun storeArrayInBuffer(array: DoubleArray): ByteBuffer {
@@ -48,4 +50,26 @@ object Util {
     fun gridSrcOf(pos: Vec2i, scale: Int): Vec2i {
         return floorDiv(pos, scale).times(scale)
     }
+
+    fun spiral(n: Int, scale: Int, srcPos: Vec2i): Vec2i {
+        return spiral(n) * scale + ((srcPos/scale)*scale)
+    }
+
+    val corners: Array<Vec2i> = arrayOf(Vec2i(1,-1), Vec2i(1,1), Vec2i(-1,1), Vec2i(-1,-1))
+    val direction: Array<Vec2i> = arrayOf(Vec2i(0,1), Vec2i(-1,0), Vec2i(0,-1), Vec2i(1,0))
+    fun spiral(n: Int): Vec2i {
+        if (n == 0)
+            return Vec2i(0)
+
+        val r = ring(n)
+        val localN = (n - area(r-1))
+        val rSizeDiv4 = (area(r)-area(r-1))/4
+        val side = localN/rSizeDiv4
+        val sideN = localN-side*rSizeDiv4 + 1
+
+        return corners[side]*r + direction[side]*sideN
+    }
+
+    fun area(n: Int) = (2*n+1)*(2*n+1)
+    fun ring(n: Int) = ceil((sqrt((n+1).toDouble())-1.0)/2.0).toInt()
 }
